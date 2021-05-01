7e6aac69041d4cf0d9d9225e96d09a65d6b4a89f
pitbox46 <37819783+pitbox46@users.noreply.github.com>
2021-05-01 10:01:18 -0400

    Fixed crashing when server has mod

--- src/main/java/github/pitbox46/musiccontroller/MusicController.java
+++ src/main/java/github/pitbox46/musiccontroller/MusicController.java
@@ -8,6 +8,7 @@ import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -16,48 +17,47 @@ import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.javafmlmod.FMLModContainer;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Path;
 
-@OnlyIn(Dist.CLIENT)
 @Mod(MusicController.MODID)
 public class MusicController {
     public static final String MODID = "musiccontroller";
-    private static final Path path = FMLLoader.getLoadingModList().getModFileById(MODID).getFile().getFilePath();
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public static final KeyBinding nextTrack = new KeyBinding("key.musiccontroller.skip_track", 79, "key.musiccontroller.category");
-    public static final KeyBinding togglePauseTrack = new KeyBinding("key.musiccontroller.pause_track", 85, "key.musiccontroller.category");
-    public static final KeyBinding toggleMusic = new KeyBinding("key.musiccontroller.toggle_music", 73, "key.musiccontroller.category");
+    public static KeyBinding nextTrack;
+    public static KeyBinding togglePauseTrack;
+    public static KeyBinding toggleMusic;
 
     public MusicController() {
-        MusicLists.init();
-        MinecraftForge.EVENT_BUS.register(new MusicGUI());
-        MinecraftForge.EVENT_BUS.register(new ScreenEvents());
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
     }
 
     private void onClientSetup(final FMLClientSetupEvent event) {
+        MusicLists.init();
+
+        MinecraftForge.EVENT_BUS.register(new MusicGUI());
+        MinecraftForge.EVENT_BUS.register(new ScreenEvents());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
+
         BackgroundMusicTracks.MAIN_MENU_MUSIC = new BackgroundMusicSelector(SoundEvents.MUSIC_MENU, Config.MENU_MUSIC_RANGE.get().get(0), Config.MENU_MUSIC_RANGE.get().get(1), true);
         BackgroundMusicTracks.CREATIVE_MODE_MUSIC = new BackgroundMusicSelector(SoundEvents.MUSIC_CREATIVE, Config.CREATIVE_MUSIC_RANGE.get().get(0), Config.CREATIVE_MUSIC_RANGE.get().get(1), false);
         BackgroundMusicTracks.END_MUSIC = new BackgroundMusicSelector(SoundEvents.MUSIC_END, Config.END_MUSIC_RANGE.get().get(0), Config.END_MUSIC_RANGE.get().get(1), true);
         BackgroundMusicTracks.UNDER_WATER_MUSIC = new BackgroundMusicSelector(SoundEvents.MUSIC_UNDER_WATER, Config.UNDER_WATER_MUSIC_RANGE.get().get(0), Config.UNDER_WATER_MUSIC_RANGE.get().get(1), false);
         BackgroundMusicTracks.WORLD_MUSIC = new BackgroundMusicSelector(SoundEvents.MUSIC_GAME, Config.GAME_MUSIC_RANGE.get().get(0), Config.GAME_MUSIC_RANGE.get().get(1), false);
 
+        nextTrack = new KeyBinding("key.musiccontroller.skip_track", 79, "key.musiccontroller.category");
+        togglePauseTrack = new KeyBinding("key.musiccontroller.pause_track", 85, "key.musiccontroller.category");
+        toggleMusic = new KeyBinding("key.musiccontroller.toggle_music", 73, "key.musiccontroller.category");
+
         ClientRegistry.registerKeyBinding(nextTrack);
         ClientRegistry.registerKeyBinding(toggleMusic);
         ClientRegistry.registerKeyBinding(togglePauseTrack);
         LOGGER.debug("Music Controller client setup");
-    }
-
-    public static Path getPath() {
-        return path;
-    }
-
-    public static Logger getLogger() {
-        return LOGGER;
     }
 }
